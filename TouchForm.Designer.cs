1ab2777150ab6065fa53ecd0c4da1a538acb80d3
igems-jesper <jesper@igems.se>
2021-02-16 11:06:29 +0100

    .

---
+++ TouchForm.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace ICTest
+{
+    partial class TouchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TouchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(790, 469);
+            this.DoubleBuffered = true;
+            this.Name = "TouchForm";
+            this.Text = "Interaction Context Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
